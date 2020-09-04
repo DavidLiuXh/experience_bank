51b7d4748df73a15a198368b2e89895171e6c92c
LiuWei <liuweixh@gmail.com>
2020-09-04 17:42:50 +0800

    add auto bat script for virtualbox
    add auto bat script  run virtualbox and startup vm

---
+++ runvb.bat
@@ -0,0 +1,11 @@
+
+cd  /d D:\Program Files (x86)\Oracel 
+start VirtualBoxVM.exe --startvm "XUbuntu" 
+
+
+ timeout /T 20 /NOBREAK 
+
+cd  /d D:\Program Files (x86)\NetSarang\Xshell 6
+start Xshell.exe -url ssh://lw:ssssss@127.0.0.1:2222
+
+goto exit
